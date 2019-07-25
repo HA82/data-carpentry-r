a102727222c864a2ace8129cd5f8d8b716728b8a
unknown <qiu-yue.zhang@students.mq.edu.au>
2019-07-25 16:07:34 +1000

    load package

---
+++ scripts/introduction.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse")
+
+library(tidyverse)
+
+print("Hello, world")
+
+
+area_hectares <- 1.0
+
+1.0-> area_hectares
+area_hectares=1
+
+print(area_hectares)
+
--- scripts/scripts.R
+++ scripts/scripts.R
@@ -1,2 +1,11 @@
 system('git config --global user.email "qiu-yue.zhang@students.mq.edu.au"')
 system('git config --global user.name "HA82"')
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", destfile = "./data/SAFI_clean.csv")
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode= "wb") 
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_openrefine.csv", destfile = "./data/SAFI_openrefine.csv", mode= "wb")
+
+
+install.packages("tidyverse")
+library(tidyverse)
+
