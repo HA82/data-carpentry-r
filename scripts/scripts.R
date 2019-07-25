fe27b2b5f75598c06b9f46577693aad1d3709191
unknown <qiu-yue.zhang@students.mq.edu.au>
2019-07-26 09:19:53 +1000

    vectors

--- scripts/scripts.R
+++ scripts/scripts.R
@@ -9,3 +9,40 @@ download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/
 install.packages("tidyverse")
 library(tidyverse)
 
+respondent_wall_type <- c("muddaub", "burntbricks", "sunbricks")
+
+
+#subseting
+
+respondent_wall_type[2]
+respondent_wall_type[c(3,2)]
+
+more_respondent_wall_type <- respondent_wall_type[c(1,2,3,2,1,3)]
+no_membrs <- c(3, 7, 10, 6)
+
+no_membrs[c(TRUE, FALSE, TRUE, TRUE)]
+
+no_membrs[no_membrs>5]
+
+
+no_membrs[no_membrs<3|no_membrs>5]
+
+
+possessions <- c("bicycle", "radio", "television")
+
+possessions[possessions == "car" | possessions == "bicycle"]
+
+possessions %in% c("car", "bicycle")
+
+rooms <- c(2,1,1,NA,4)
+mean(rooms)
+max(rooms)
+
+mean(rooms, na.rm=TRUE)
+
+is.na(rooms)
+
+!is.na(rooms)
+
+rooms[!is.na(rooms)]
+
